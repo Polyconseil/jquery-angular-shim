dfc8c38a9318ba6ecbff1622296ac7d6de34c109
Cédric Codet <cedric.codet@polyconseil.fr>
2016-06-16 15:03:06 +0200

    Also save original jQuery reference

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 function load () {
   if (!window.__jquery_angular_shim__) {
+    // avoid conflicts; keep old angular and jQuery references.
+    var originalJQuery = window.jQuery
+    var originalAngular = window.angular
+
     var jQuery = require('jquery')
-    var originalAngular = window.angular  // avoid conflicts; keep old angular reference.
     window.angular = null                 // trick angular into loading itself and not just return the global one.
     var angular = require('angular')
     window.__jquery_angular_shim__ = {
       jquery: jQuery,
       angular: angular,
       originalAngular: originalAngular,
+      originalJQuery: originalJQuery,
     }
   }
   return window.__jquery_angular_shim__.angular
